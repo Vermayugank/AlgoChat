dfb2cd4d524435cc656d1e0e2e7644a48224c2ba
Harsh Kumar <harshkk.ug20.ee@nitp.ac.in>
2023-02-08 20:28:47 +0530

    harsh's commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import buildspaceLogo from "../assets/buildspace-logo.png";
-import linkedinLogo from "../assets/linkedin-logo.png";
+import algoLogo from "../assets/algo-logo.png";
 import { useState } from "react";
 import Chat from "./components/Chat";
 import Navbar from "./components/Navbar";
@@ -10,26 +9,7 @@ const Home = () => {
   const [userInput, setUserInput] = useState("");
   const [apiOutput, setApiOutput] = useState("");
   const [isGenerating, setIsGenerating] = useState(false);
-
-  // const callGenerateEndpoint = async () => {
-  //   setIsGenerating(true);
-
-  //   console.log("Calling OpenAI...");
-  //   const response = await fetch("/api/generate", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ userInput }),
-  //   });
-
-  //   const data = await response.json();
-  //   const { output } = data;
-  //   console.log("OpenAI replied...", output.text);
-
-  //   setApiOutput(`${output.text}`);
-  //   setIsGenerating(false);
-  // };
+  
 
   const getReply = async (ques) => {
     const response = await fetch("/api/generate", {
@@ -54,64 +34,20 @@ const Home = () => {
       {/* <Navbar /> */}
       <Chat getReply={getReply}/>
 
-      {/* <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>Question Bot</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>Ask him about</h2>
-          </div>
-        </div>
-
-        <div className="prompt-container">
-          <textarea
-            placeholder="start typing here"
-            className="prompt-box"
-            value={userInput}
-            onChange={onUserChangedText}
-          />
-          <div className="prompt-buttons">
-            <a
-              className={
-                isGenerating ? "generate-button loading" : "generate-button"
-              }
-              onClick={callGenerateEndpoint}
-            >
-              <div className="generate">
-                {isGenerating ? (
-                  <span className="loader"></span>
-                ) : (
-                  <p>Generate</p>
-                )}
-              </div>
-            </a>
-          </div>
-          {apiOutput && (
-            <div className="output">
-              <div className="output-header-container">
-                <div className="output-header">
-                  <h3>Output</h3>
-                </div>
-              </div>
-              <div className="output-content">
-                <p>{apiOutput}</p>
-              </div>
-            </div>
-          )}
-        </div>
+      {/*
       </div> */}
 
 
       <div className="badge-container grow">
         <a
-          href="https://www.linkedin.com/in/yugank-verma-4376b2201"
+          href="https://developer.algorand.org/docs/"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={linkedinLogo} alt="Linkedin logo" />
-            <p>Linkedin-Yugank Verma</p>
+            <Image src={algoLogo} alt="Algorand Logo" />
+            <p>Algorand Docs</p>
+            
           </div>
         </a>
       </div>
